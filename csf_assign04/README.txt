61fc32f77f44a19fb47286e6f2bd3b9c6fb78fb2
Lauren Siu <lsiu1@ugradx.cs.jhu.edu>
2023-11-10 19:10:58 -0500

    test autograder

--- csf_assign04/README.txt
+++ csf_assign04/README.txt
@@ -1,13 +1,53 @@
 CONTRIBUTIONS
 
-TODO: write a brief summary of how each team member contributed to
-the project.
+Alan and Lauren worked together to write the assignment together as a whole. 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 55962b44a878d2bcfee5feb0e3a6478b1fad7ac0
 REPORT
+Experiments and Analysis: 
+Analysis: 
+From the data below, we can see that a lower threshold produces a lower runtime which we can conclude that 
+creating more parallel processes can decrease the runtime to some degree. As the number of parallel processes 
+increase infinitely, the runtime will stop decreasing relatively and increase. This is because of how inherently 
+merge sort works as it has O(n*logn) time complexity.
 
-TODO: add your report according to the instructions in the
-"Experiments and analysis" section of the assignment description.
+Data:
+Test run with threshold 2097152
+
+real    0m0.998s
+user    0m0.950s
+sys     0m0.036s
+Test run with threshold 1048576
+
+real    0m0.641s
+user    0m0.947s
+sys     0m0.047s
+Test run with threshold 524288
+
+real    0m0.765s
+user    0m1.369s
+sys     0m0.131s
+Test run with threshold 262144
+
+real    0m1.051s
+user    0m2.285s
+sys     0m0.259s
+Test run with threshold 131072
+
+real    0m1.633s
+user    0m4.004s
+sys     0m0.618s
+Test run with threshold 65536
+
+real    0m2.470s
+user    0m7.180s
+sys     0m1.376s
+Test run with threshold 32768
+
+real    0m3.522s
+user    0m13.035s
+sys     0m3.399s
+Test run with threshold 16384
+
+real    0m5.430s
+user    0m25.958s
+sys     0m9.379s
--- csf_assign04/parsort.c
+++ csf_assign04/parsort.c
@@ -10,6 +10,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+/** 
+ * Function that compares two values together 
+ * 
+ * Parameters:
+ *  a - first value
+ *  b - second value
+ * 
+ * Returns:
+ *  -1 if first value is less than second 
+ *  1 if second is less than first
+ *  0 if equal
+**/ 
 int compare_i64(const void *left_, const void *right_) {
   int64_t left = *(int64_t *)left_;
   int64_t right = *(int64_t *)right_;
@@ -18,13 +30,29 @@ int compare_i64(const void *left_, const void *right_) {
   return 0;
 }
 
+/**
+ * Sorts a sequence of int64_t elements in ascending order using quicksort
+ *
+ * Parameters:
+ *  arr - pointer to the array of int64_t elements.
+ *  begin - the starting index of the subarray to be sorted (inclusive).
+ *  end - the ending index of the subarray to be sorted (exclusive).
+ */
 void seq_sort(int64_t *arr, size_t begin, size_t end) {
   size_t num_elements = end - begin;
   qsort(arr + begin, num_elements, sizeof(int64_t), compare_i64);
 }
 
-// Merge the elements in the sorted ranges [begin, mid) and [mid, end),
-// copying the result into temparr.
+/**
+ * Merges two sorted halves of an array into a single sorted array.
+ * 
+ * Parameters:
+ *  arr - the input array containing the elements to be merged.
+ *  begin - the starting index of the left subarray.
+ *  mid - the ending index of the left subarray and the starting index of the right subarray.
+ *  end - the ending index of the right subarray.
+ *  temparr - temporary array to store the merged elements.
+*/
 void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr) {
   int64_t *endl = arr + mid;
   int64_t *endr = arr + end;
@@ -52,11 +80,25 @@ void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr)
 
 void fatal(const char *msg) __attribute__ ((noreturn));
 
+/**
+ * Prints an error message to the standard error stream and exits the program with a status code of 1.
+ *
+ * Parameters:
+ *  msg - a pointer to a null-terminated string containing the error message to be displayed.
+ */
 void fatal(const char *msg) {
   fprintf(stderr, "Error: %s\n", msg);
   exit(1);
 }
 
+/**
+ * Performs parallel merge sort on the given array within the specified range.
+ * Parameters:
+ *  arr - pointer to the array to be sorted.
+ *  begin - starting index of the range to be sorted (inclusive).
+ *  end - ending index of the range to be sorted (exclusive).
+ *  threshold - the threshold at which the sorting switches to a sequential method.
+ */
 void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   assert(end >= begin);
   size_t size = end - begin;
@@ -70,15 +112,79 @@ void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
 
   size_t mid = begin + size/2;
 
-  // TODO: parallelize the recursive sorting
+  //parallelize the recursive sorting
+
+  //flag to check that errors propagate upwards 
+  int prop_flag = 0; 
+
+  if((size) <= threshold){
+    qsort(arr + begin, size, sizeof(int64_t), compare_i64);
+    return; 
+  } else { //greater than threshold, recursively call merge with child process
+    pid_t pid1 = fork(); //create first child process
+    if (pid1 == -1) {
+      fatal("Error: fork failed\n"); 
+    } else if(pid1 == 0) {
+      merge_sort(arr, begin, mid, threshold); 
+      exit(0);
+    }
+
+    pid_t pid2 = fork(); //create second child process
+    if (pid2 == -1) {
+      fatal("Error: fork failed\n");
+    } else if(pid2 == 0) {
+      merge_sort(arr, mid, end, threshold); 
+      exit(0); 
+    }
+
+    int wstatus1; //wait for child process completion
+    pid_t pid_real = waitpid(pid1, &wstatus1, 0); 
+    if (pid_real == -1) {
+      fprintf(stderr, "Error: waitpid1 failed!\n"); 
+      prop_flag = 1; 
+    }
+
+    int w2status; //wait for child process completion
+    pid_real = waitpid(pid2, &w2status, 0); 
+    if (pid_real == -1) {
+      fprintf(stderr, "Error: waitpid2 failed!\n"); 
+      prop_flag = 1; 
+    }
+
+    if (!WIFEXITED(wstatus1)) {
+      fprintf(stderr, "Error: subprocess1 crashed, was interrupted, or did not exit normally\n");
+      prop_flag = 1; 
+    }
+    if (WEXITSTATUS(wstatus1) != 0) {
+      fprintf(stderr, "Error: subprocess1 returned a non-zero exit code\n"); 
+      prop_flag = 1; 
+    }
+
+    if (!WIFEXITED(w2status)) {
+      fprintf(stderr, "Error: subprocess2 crashed, was interrupted, or did not exit normally\n");
+      prop_flag = 1; 
+    }
+    if (WEXITSTATUS(w2status) != 0) {
+      fprintf(stderr, "Error: subprocess2 returned a non-zero exit code\n"); 
+      prop_flag = 1; 
+    }
+  
+  }
+
+  if (prop_flag) { //check that no errors occured in child processes
+    fatal("Error: child processes did not proceed properly\n"); 
+    exit(EXIT_FAILURE); 
+  }
+
   merge_sort(arr, begin, mid, threshold);
   merge_sort(arr, mid, end, threshold);
 
   // allocate temp array now, so we can avoid unnecessary work
   // if the malloc fails
   int64_t *temp_arr = (int64_t *) malloc(size * sizeof(int64_t));
-  if (temp_arr == NULL)
+  if (temp_arr == NULL) {
     fatal("malloc() failed");
+  }
 
   // child processes completed successfully, so in theory
   // we should be able to merge their results
@@ -106,18 +212,48 @@ int main(int argc, char **argv) {
   char *end;
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
   if (end != argv[2] + strlen(argv[2])) {
-    // TODO: report an error (threshold value is invalid)
+    // report an error (threshold value is invalid)
+    fatal("Error: Threshold value is invalid\n");
   }
 
-  // TODO: open the file
+ // open the file
+  int fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    // report an error (fle cannot be opened)
+    fatal("Error: file couldn't be opened\n");
+  }
+ 
+  //use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+    //report an error (fstat error)
+    fatal("Error: Bad fstat\n");
+  }
 
-  // TODO: use fstat to determine the size of the file
+  size_t file_size_in_bytes = statbuf.st_size;
+  
+  // map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+    fatal("Error: Bad map\n");
+  }
+  
+  //sort the data!
+  uint64_t num_el = file_size_in_bytes/8;
+  merge_sort(data, 0, num_el,threshold);
 
-  // TODO: map the file into memory using mmap
-
-  // TODO: sort the data!
-
-  // TODO: unmap and close the file
-
-  // TODO: exit with a 0 exit code if sort was successful
+  //unmap and close the file
+  int um = munmap(data, file_size_in_bytes); 
+  if (um != 0) {
+    fatal("Error: could not unmap file\n");
+  }
+  
+  //exit with a 0 exit code if sort was successful
+  int c = close(fd);
+  if (c != 0) {
+    fatal("Error: could not close file\n");
+  }
+  //exit with a 0 exit code if sort was successful
+  exit(EXIT_SUCCESS);
 }
