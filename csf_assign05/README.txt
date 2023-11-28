cf131693f652b907d5ed2112b73f6b16e118cf69
am12 <smao10@jhu.edu>
2023-11-27 22:40:39 -0500

    finished sender

--- csf_assign05/README.txt
+++ csf_assign05/README.txt
@@ -1,4 +1,9 @@
-Sample README.txt
+CONTRIBUTIONS
+
+MS1: Alan and Lauren worked together to implement the various client functions. Alan worked on receiver.cpp while Lauren worked on sender.cpp.
+MS2: 
+
+REPORT
 
 Eventually your report about how you implemented thread synchronization
 in the server should go here
--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -21,12 +21,62 @@ int main(int argc, char **argv) {
   server_port = std::stoi(argv[2]);
   username = argv[3];
 
-  // TODO: connect to server
+  // connect to server
+  Connection conn;
+  conn.connect(server_hostname, server_port);
 
-  // TODO: send slogin message
+  // check if connection was successful
+  if (!conn.is_open()) {
+    std::cerr << "Connection failed!";
+    return 1;
+  }
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
+  // send slogin message
+  conn.send(Message(TAG_SLOGIN, username));
+  
+  Message slogin_response = Message();
+  conn.receive(slogin_response);
 
+  if (slogin_response.tag == TAG_ERR) {
+    std::cerr << slogin_response.data;
+    conn.close();
+    return 1;
+  }
+
+  // loop reading commands from user, sending messages to server as appropriate
+  while(1) {
+
+    Message message;
+    std::string input;
+    std::getline(std::cin, input);
+
+    if (input.substr(0, 6) == "/join ") {
+      message.tag = TAG_JOIN;
+      message.data = input.substr(6);
+    } else if (input == "/leave") {
+      message.tag = TAG_LEAVE;
+    } else if (input == "/quit") {
+      message.tag = TAG_QUIT;
+      conn.send(message);
+      Message quit_response = Message();
+      conn.receive(quit_response);
+      if (quit_response.tag == TAG_ERR || conn.get_last_result() == Connection::INVALID_MSG) {
+        std::cerr << quit_response.data;
+      }
+      break;
+    } else {
+      message.tag = TAG_SENDALL;
+      message.data = input;
+    }
+    conn.send(message);
+    Message message_response = Message();
+    conn.receive(message_response);
+    if (message_response.tag == TAG_ERR || conn.get_last_result() == Connection::INVALID_MSG) {
+      std::cerr << message_response.data;
+      continue;
+    }
+  }
+
+  conn.close();
   return 0;
 }
